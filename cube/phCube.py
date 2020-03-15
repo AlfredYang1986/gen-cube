f4e4c3f0ab9200c0f455d20ad4cb8146e3de93df
AlfredYang1986 <yangyuanpig@gmail.com>
2020-03-15 13:24:22 +0800

    重构一下

---
+++ cube/phCube.py
@@ -0,0 +1,145 @@
+# coding=utf-8
+"""
+Created on Fri Mar 13 19:57:27 2020
+
+@author: Alfred Yang
+"""
+
+import pandas as pd
+import itertools
+import json
+from functions import cartesian
+
+
+class PhCube(object):
+    def __init__(self, frame_path, dimension_path, output_path):
+        self.data = pd.read_csv(frame_path)
+        """
+        2. construction of data dimension 
+        """
+        with open(dimension_path, "r") as f:
+            dm = json.loads(f.read())
+        self.dms = dm["dimensions"]
+        self.all_hierarchies = self.gen_all_hierarchies()
+        self.cuboids = []
+        self.output = output_path
+
+    def gen_cuboids(self):
+        if len(self.cuboids) > 0:
+            self.cuboids = []
+
+        for ld in range(len(self.dms) + 1):
+            # 1. for all dimensions in these example is 3-D cuboid or base cuboid
+            # 1.1 for n-dimension should have 2^n cuboids (or panels)
+            #     in this place I am using the combination (排列组合)
+            for cuboid in itertools.combinations(self.dms, ld):
+                # 1.2 construct bitmap dimension indexing
+                #     also can be save the last when you want to build indexing of the cuboids
+                self.cuboids.append(cuboid)
+
+        return self.cuboids
+
+    def gen_all_hierarchies(self):
+        tmp = []
+        for dm in self.dms:
+            for her in dm["hierarchy"]:
+                tmp.append(her)
+
+        self.all_hierarchies = tmp
+        return tmp
+
+    def get_all_hierarchies(self):
+        return self.all_hierarchies
+
+    # 2. for all cuboid, build lattices base on the dimension's concept hierarchies
+    def gen_cube_data(self):
+        for cuboid in self.cuboids:
+            self.gen_cuboids_data(cuboid)
+
+    def gen_cuboids_data(self, cuboid):
+        # 2.1 create metadata for the cuboid
+        #    2.1.1 the name of the cuboid for now only
+        def gen_cuboid_name():
+            dimensions_names = []
+            for dimension in cuboid:
+                dimensions_names.append(dimension["name"])
+            # print dimensions_names
+            dimension_count = len(cuboid)
+            return "apex" \
+                if dimension_count == 0 \
+                else "base" if dimension_count == len(self.dms) \
+                else str(dimension_count) + "-D-" + "-".join(dimensions_names)
+
+        # 3. construction for the lattices of the cuboid
+        #   3.1 construction all the combination in the dimension concept hierarchies
+        #       for 1-D dimension, the lattice number of the cuboid is calculated by the number of the concept hierarchy
+        #       for 2-D dimension cuboid, which has n and m concept hierarchy for each dimension especially,
+        #       the number of the lattices is m * n
+        #       in summary, n-dimension with n levels of hierarchies would have n^n lattice.
+        #       in this example, we use cartesian to implement
+        def gen_cur_hierarchies():
+            result = []
+            for har in cuboid:
+                result.append(har["hierarchy"])
+            return result
+
+        # 3.2 for apex lattice, replace dimension with * character
+        #     star node
+        def gen_apex_lattice():
+            all_hierarchies = self.get_all_hierarchies()
+            # for col in all_hierarchies:
+            self.data.loc[:, "apex"] = "alfred"
+            apex = self.data.groupby(["apex"])["SALES_QTY", "SALES_VALUE"].sum()
+            # 3.3 fill dimension columns
+            for hier in all_hierarchies:
+                apex[hier] = "*"
+            # 3.4 fill cube metadata
+            apex["dimension.name"] = "apex"
+            apex["dimension.value"] = "*"
+            apex = apex.reset_index(drop=True)
+            apex.to_csv(self.output + cuboid_name + ".csv")
+
+        def gen_base_lattice():
+            self.data.to_csv(self.output + cuboid_name + ".csv")
+
+        # 4. for calculation lattice in  n-dimension cuboids,
+        #    group by the higher level hierarchy in such dimension
+        def gen_dimension_lattice():
+            for cartesian_hierarchies in cartesian(hierarchies):
+                group_hierarchy = []
+                for tmp_hierarchy in cartesian_hierarchies:
+                    for tmp_dimension in cuboid:
+                        try:
+                            for index in range(tmp_dimension["hierarchy"].index(tmp_hierarchy) + 1):
+                                group_hierarchy.append(tmp_dimension["hierarchy"][index])
+                        except ValueError, e:
+                            print "not in the array"
+                data_lattice = self.data.groupby(group_hierarchy)["SALES_QTY", "SALES_VALUE"].sum().reset_index()
+                # 4.1 fill the other
+                fill_hierarchies = []
+                for dm in self.dms:
+                    for her in dm["hierarchy"]:
+                        if her not in group_hierarchy:
+                            fill_hierarchies.append(her)
+                # print fill_hierarchies
+                for fill_hier in fill_hierarchies:
+                    data_lattice[fill_hier] = "*"
+                # 4.4 fill cube metadata
+                data_lattice["dimension.name"] = cuboid_name
+                data_lattice["dimension.value"] = ""
+                for condi in cartesian_hierarchies:
+                    data_lattice.loc[:, "dimension.value"] = \
+                        data_lattice.loc[:, "dimension.value"].apply(lambda x: str(x)) + "," + \
+                        data_lattice.loc[:, condi.encode("utf8")].apply(lambda x: condi.encode("utf8") + ":" + str(x))
+                data_lattice.to_csv(self.output + cuboid_name + "-"
+                                    + "-".join(cartesian_hierarchies) + ".csv")
+
+        cuboid_name = gen_cuboid_name()
+        hierarchies = gen_cur_hierarchies()
+
+        if len(hierarchies) == 0:
+            gen_apex_lattice()
+        elif len(hierarchies) == len(self.dms):
+            gen_base_lattice()
+        else:
+            gen_dimension_lattice()
--- main.py
+++ main.py
@@ -10,9 +10,8 @@ import os
 import sys
 
 import pandas as pd
-import numpy as np
-from dataGen import ExcelData
-from utils import cartesian
+from dataCleaning import ExcelData
+from cube import PhCube
 
 
 """
@@ -79,113 +78,6 @@ if test and os.path.isfile(frame):
             sys.stderr.write("Every measure should have no null or NaN")
             exit(-1)
 
-"""
-2. construction of data dimension 
-"""
-with open(dimensions, "r") as f:
-    dm = json.loads(f.read())
-
-dms = dm["dimensions"]
-
-# 1. for all dimensions in these example is 3-D cuboid or base cuboid
-# print dms
-# 1.1 for n-dimension should have 2^n cuboids (or panels)
-#     in this place I am using the combination (排列组合)
-cuboids = []
-for ld in range(len(dms) + 1):
-    for cuboid in itertools.combinations(dms, ld):
-        # 1.2 construct bitmap dimension indexing
-        #     also can be save the last when you want to build indexing of the cuboids
-        cuboids.append(cuboid)
-# print cuboids
-
-# 2. for all cuboid, build lattices base on the dimension's concept hierarchies
-for cuboid in cuboids:
-    # print cuboid
-    # 2.1 create metadata for the cuboid
-    #    2.1.1 the name of the cuboid for now only
-    dimensions_names = []
-    for dimension in cuboid:
-        dimensions_names.append(dimension["name"])
-    # print dimensions_names
-    dimension_count = len(cuboid)
-    cuboid_name = "apex" \
-        if dimension_count == 0 \
-        else "base" if dimension_count == len(dms) \
-        else str(dimension_count) + "-D-" + "-".join(dimensions_names)
-    # print cuboid_name
-
-    # 3. construction for the lattices of the cuboid
-    #   3.1 construction all the combination in the dimension concept hierarchies
-    #       for 1-D dimension, the lattice number of the cuboid is calculated by the number of the concept hierarchy
-    #       for 2-D dimension cuboid, which has n and m concept hierarchy for each dimension especially,
-    #       the number of the lattices is m * n
-    #       in summary, n-dimension with n levels of hierarchies would have n^n lattice.
-    #       in this example, we use cartesian to implement
-    hierarchies = []
-    for har in cuboid:
-        hierarchies.append(har["hierarchy"])
-
-    if len(hierarchies) == 0:
-        # print "apex"
-        # 3.2 for apex lattice, replace dimension with * character
-        #     star node
-        all_hierarchies = []
-        for dm in dms:
-            for her in dm["hierarchy"]:
-                all_hierarchies.append(her)
-        for col in all_hierarchies:
-            data.loc[:, "apex"] = "alfred"
-            apex = data.groupby(["apex"])["SALES_QTY", "SALES_VALUE"].sum()
-            # 3.3 fill dimension columns
-            for hier in all_hierarchies:
-                apex[hier] = "*"
-            # 3.4 fill cube metadata
-            apex["dimension.name"] = "apex"
-            apex["dimension.value"] = "*"
-            apex.to_csv(output + cuboid_name + ".csv")
-    elif len(hierarchies) == len(dms):
-        # print "base"
-        data.to_csv(output + cuboid_name + ".csv")
-    else:
-        for cartesian_hierarchies in cartesian(hierarchies):
-            # print "alfred"
-            # print cartesian_hierarchies
-            # 4. for calculation lattice in  n-dimension cuboids,
-            #    group by the higher level hierarchy in such dimension
-            group_hierarchy = []
-            for tmp_hierarchy in cartesian_hierarchies:
-                for tmp_dimension in cuboid:
-                    try:
-                        for index in range(tmp_dimension["hierarchy"].index(tmp_hierarchy) + 1):
-                            group_hierarchy.append(tmp_dimension["hierarchy"][index])
-                    except ValueError, e:
-                        # do nothing
-                        print "not in the array"
-
-            # print group_hierarchy
-            data_lattice = data.groupby(group_hierarchy)["SALES_QTY", "SALES_VALUE"].sum().reset_index()
-            # print data_lattice.reset_index()
-
-            # 4.1 fill the other
-            fill_hierarchies = []
-            for dm in dms:
-                for her in dm["hierarchy"]:
-                    if her not in group_hierarchy:
-                        fill_hierarchies.append(her)
-            # print fill_hierarchies
-
-            for fill_hier in fill_hierarchies:
-                data_lattice[fill_hier] = "*"
-
-            # 4.4 fill cube metadata
-            data_lattice["dimension.name"] = cuboid_name
-            data_lattice["dimension.value"] = ""
-
-            for condi in cartesian_hierarchies:
-                data_lattice.loc[:, "dimension.value"] = \
-                    data_lattice.loc[:, "dimension.value"].apply(lambda x: str(x)) + "," + \
-                    data_lattice.loc[:, condi.encode("utf8")].apply(lambda x: condi.encode("utf8") + ":" + str(x))
-
-            data_lattice.to_csv(output + cuboid_name + "-".join(cartesian_hierarchies) + ".csv")
-
+cube = PhCube(frame_path=frame, dimension_path=dimensions, output_path=output)
+cube.gen_cuboids()
+cube.gen_cube_data()
